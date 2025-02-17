3421a4b299dce6fda5659f516cc8be949cfdfcb4
Lucimar Ferreira <lucimar.silvasr61@gmail.com>
2025-02-17 19:24:39 -0300

    Atualização da tela de cadastro de produtos, inserção de popUp para confirmação de cadastro.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,21 @@
+// Deixando o menu sidebar dinâmico
 document.getElementById('open_btn').addEventListener('click', function() {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+// Exibindo pop up na tela de cadastro de produtos
+function abrirPopUp() {
+    document.getElementById("popUp").style.display = "block";
+}
+
+function fecharPopUp() {
+    document.getElementById("popUp").style.display = "none";
+}
+
+// Fechar ao clicar fora do modal
+window.onclick = function(event) {
+    let modal = document.getElementById("popUp");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
